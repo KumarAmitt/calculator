75685d0a500cd692609aeac30aa5bb5bdf700fe5
KumarAmitt <amitt2015@outlook.com>
2021-06-27 14:04:31 +0530

    Fix linter Offenses

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello React</h1>;
-  }
-}
+const Hello = () => <h1>Hello React</h1>;
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
