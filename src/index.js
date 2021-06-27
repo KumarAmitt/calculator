3d4dcaa1d096f25c520207e25fa493e57a31fce7
KumarAmitt <amitt2015@outlook.com>
2021-06-27 13:56:54 +0530

    Setup linters

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Hello extends React.Component {
-
-  render(){
-    return <h1>Hello React</h1>
+  render() {
+    return <h1>Hello React</h1>;
   }
-
 }
 
-ReactDOM.render(<Hello />, document.getElementById('root'))
+ReactDOM.render(<Hello />, document.getElementById('root'));
