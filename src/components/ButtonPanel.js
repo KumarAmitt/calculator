81293b34a2042b503c4aa145349f6a1dcf41b780
KumarAmitt <amitt2015@outlook.com>
2021-06-29 18:45:46 +0530

    Add clickHandler in buttonPanel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,40 +1,46 @@
 import React from 'react';
 import Button from './Button';
 
-const ButtonPanel = () => (
-  <>
-    <section className="button-panel">
-      <div>
-        <Button buttonName="AC" />
-        <Button buttonName="+/-" />
-        <Button buttonName="%" />
-        <Button buttonName="÷" />
-      </div>
-      <div>
-        <Button buttonName="7" />
-        <Button buttonName="8" />
-        <Button buttonName="9" />
-        <Button buttonName="X" />
-      </div>
-      <div>
-        <Button buttonName="4" />
-        <Button buttonName="5" />
-        <Button buttonName="6" />
-        <Button buttonName="-" />
-      </div>
-      <div>
-        <Button buttonName="1" />
-        <Button buttonName="2" />
-        <Button buttonName="3" />
-        <Button buttonName="+" />
-      </div>
-      <div>
-        <Button buttonName="0" />
-        <Button buttonName="." />
-        <Button buttonName="=" />
-      </div>
-    </section>
-  </>
-);
+const ButtonPanel = (clickHandler) => {
+  const handleClick = (buttonName) => {
+    clickHandler(buttonName);
+  };
+
+  return (
+    <>
+      <section className="button-panel">
+        <div>
+          <Button buttonName="AC" clickHandler={handleClick} />
+          <Button buttonName="+/-" clickHandler={handleClick} />
+          <Button buttonName="%" clickHandler={handleClick} />
+          <Button buttonName="÷" clickHandler={handleClick} />
+        </div>
+        <div>
+          <Button buttonName="7" clickHandler={handleClick} />
+          <Button buttonName="8" clickHandler={handleClick} />
+          <Button buttonName="9" clickHandler={handleClick} />
+          <Button buttonName="X" clickHandler={handleClick} />
+        </div>
+        <div>
+          <Button buttonName="4" clickHandler={handleClick} />
+          <Button buttonName="5" clickHandler={handleClick} />
+          <Button buttonName="6" clickHandler={handleClick} />
+          <Button buttonName="-" clickHandler={handleClick} />
+        </div>
+        <div>
+          <Button buttonName="1" clickHandler={handleClick} />
+          <Button buttonName="2" clickHandler={handleClick} />
+          <Button buttonName="3" clickHandler={handleClick} />
+          <Button buttonName="+" clickHandler={handleClick} />
+        </div>
+        <div>
+          <Button buttonName="0" clickHandler={handleClick} />
+          <Button buttonName="." clickHandler={handleClick} />
+          <Button buttonName="=" clickHandler={handleClick} />
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default ButtonPanel;
