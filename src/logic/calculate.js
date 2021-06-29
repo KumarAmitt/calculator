9ad5f5283092b8cbb77153532f53711935db0255
KumarAmitt <amitt2015@outlook.com>
2021-06-29 14:38:00 +0530

    Add operation for operate function

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,5 +1,5 @@
-const calculate = () => {
-
+const calculate = (data, buttonName) => {
+  const { total, next, operation } = data;
 };
 
 export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,5 +1,33 @@
-const operate = () => {
+import Big from 'big.js';
 
+const operate = (numberOne, numberTwo, operation) => {
+  if (!numberOne || !numberTwo || !operation) return '0';
+
+  const num1 = Big(numberOne);
+  const num2 = Big(numberTwo);
+  let result;
+
+  switch (operation) {
+    case '+':
+      result = num1.plus(num2);
+      break;
+    case '-':
+      result = num1.minus(num2);
+      break;
+    case 'X':
+      result = num1.times(num2);
+      break;
+    case '÷':
+      result = num1.div(num2);
+      break;
+    case '%':
+      result = (num1.times(num2)).div(100);
+      break;
+    default:
+      result = 0;
+  }
+
+  return result.toString();
 };
 
 export default operate;
