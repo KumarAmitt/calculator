8b510dd6b94caf109def4489b30cc4b16d58e615
KumarAmitt <amitt2015@outlook.com>
2021-06-29 18:27:06 +0530

    Convert App to a class component and add handleClick method

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// import calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display result="0" />
-    <ButtonPanel />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { total, next, operation } = calculate(this.state, buttonName);
+    this.setState({ total, next, operation });
+  }
+
+  render() {
+    return (
+      <>
+        <Display result={this.total} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </>
+    );
+  }
+}
 
 export default App;
