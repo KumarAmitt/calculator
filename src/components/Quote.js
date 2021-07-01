01c85cc07968f9346c520f3c5e1234417b40556a
KumarAmitt <amitt2015@outlook.com>
2021-07-01 23:51:33 +0530

    Add quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Quote = () => {
+  const quote = 'Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding.';
+  const cite = 'William Paul Thurston';
+  return (
+    <>
+      <blockquote>
+        {quote}
+        <cite>
+          &mdash;
+          {cite}
+        </cite>
+      </blockquote>
+    </>
+  );
+};
+
+export default Quote;
