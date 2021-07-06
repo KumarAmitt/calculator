8b8312d3c5940fbab40d636b1ee52a242caf76f2
KumarAmitt <amitt2015@outlook.com>
2021-07-06 14:53:41 +0530

    Add test for Display component

---
+++ src/components/__test__/Home.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+import Home from '../Home';
+
+test('should have a greeting text', () => {
+  const { getByTestId } = render(<Home />);
+  const greetElement = getByTestId('greet');
+
+  expect(greetElement.textContent).toBe('Welcome to our page!');
+});
+
+test('test for the paragraph element', () => {
+  const tree = renderer.create(<Home />);
+
+  expect(tree).toMatchSnapshot();
+});
